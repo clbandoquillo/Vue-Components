9c4594d7ca21ca40c85b04fa3989133da34a9fa0
clbandoquillo <bandoquillo.c33@gmail.com>
2020-01-10 22:12:41 +0800

    finished

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <server-heading></server-heading>
+        <hr>
+        <div class="row">
+            <server-list></server-list>
+            <server-details></server-details>
+        </div>
+        <hr>
+        <server-info></server-info>
+    </div>
+</template>
+
+<script>
+    import ServerList from './ServerList.vue';
+    import PageInfo from './PageInfo.vue';
+    import PageHeading from './PageHeading.vue';
+    import ServerDetails from './ServerDetails.vue';
+
+    export default{
+
+        components: {
+            'server-info' : PageInfo,
+            'server-list' : ServerList,
+            'server-heading' : PageHeading,
+            'server-details' : ServerDetails
+        }
+    }
+
+</script>
+
+<style>
+
+</style>
---
+++ src/ServerDetails.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="row">
+        <div class="col-xs-12 col-sm-6">
+            <header>
+                <h1>Server Details are currently not updated</h1>
+            </header>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        
+    }
+</script>
